8464d9ae0e2845be0e3cf1e60e950d4bd1e1a720
Juan Francisco Villasmil Tovar <juanvillans16@gmail.com>
2024-09-23 15:02:12 -0400

    ni

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,14 @@
 	import Alert from "$lib/components/Alert.svelte";
     import {displayAlert} from "../stores/alertStore"
 
-	export let loginResponse;
-	let ci = '';
-	let password = '';
+	export let form;
+	let password = ""
+	let ci= ""
+	console.log({form})
 	let valueSubmit = 'ENTRAR'
-	$: console.log(ci, password);
-
+	$: if (form?.error) {
+		displayAlert({type: "error", message: form?.message})
+	}
 	async function handleSubmit() {
 
 	}
@@ -36,9 +38,9 @@
 			>
 				<legend class=" opacity-70 mb-6">INICIAR SESIÓN</legend>
 				<div>
-					<Input type={'number'} label={'Cédula'} name="ci" bind:value={email} classes={'mb-5'} />
-					<!-- <input bind:value={email}> -->
-					<Input type={'password'} label={'Contraseña'} name="password" bind:value={password} />
+					<Input type={'number'} label={'Cédula'} name="ci" value={form?.ci} classes={'mb-5'} />
+					<!-- <input value={ci}> -->
+					<Input type={'password'} label={'Contraseña'} name="password" value={form?.password} />
 				</div>
 				<input
 					type="submit"
